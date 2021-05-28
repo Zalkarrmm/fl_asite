36329cc851769cb963e90df01b9d6048ea7b7cef
Zalkarrmm <zalkarrmm@gmail.com>
2021-05-28 06:32:48 +0600

    maket

--- js/script.js
+++ js/script.js
@@ -14,16 +14,15 @@ noneBtn.addEventListener('click' , e => {
 // second
 const collaplseBtn = document.querySelectorAll('.getCollapse');
 const commits = document.querySelector('.collapsed')
-const commits2 = document.querySelector('.collapsed2')
-
 const commitz = document.querySelector('.collapsedd')
-const commitz2 = document.querySelector('.collapsedd2')
+const commites = document.querySelector('.collapsedd1')
 
 function getCollapse(){
     commits.classList.toggle('d-block')
-    commits2.classList.toggle('d-block')
 }
 function getCollapsed(){
     commitz.classList.toggle('d-block')
-    commitz2.classList.toggle('d-block')
+}
+function getCollapsede(){
+    commites.classList.toggle('d-block')
 }
